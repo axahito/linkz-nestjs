cad1b94517d57e0632b3f2392a6b49bc8fc132e7
Chris <abiyyuakahfi@gmail.com>
2024-04-28 22:22:17 +0700

    footer text responsive

--- client/src/components/layout/footer/Footer.tsx
+++ client/src/components/layout/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return <footer className="bg-gray-800 text-white p-4 flex justify-center items-center">Test Submission for Linkz.asia</footer>;
+  return <footer className="bg-gray-800 text-white p-4 flex justify-center items-center text-xs md:text-sm">Test Submission for Linkz.asia</footer>;
 }
 
 export default Footer;
